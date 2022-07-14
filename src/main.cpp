ef165e98d735abc2f17c20599e075f60ec041436
Andrisetianabrata <andri.setiana.brata69@gmail.com>
2022-07-14 16:17:01 +0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ void setup()
 {
   setting_up();
   Buzz.init_pin = D7;
-  Selenoid.init_pin = D8;
+  Selenoid.init_pin = D0;
   Pintu.init_pin = D1;
   Buzz.setpin_output();
   Pintu.setpin_input();
